c15420af4bf41191b80f6b27c380fde70135ff05
Thivyan <thivyan6@gmail.com>
2025-03-12 01:58:37 +0300

    Added a UI with buttons and JS manipulating the HTML.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
 /* 
 
-Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+Your game will be played against the computer. 
+You will write a function that randomly returns “rock”, “paper” or “scissors”.
 
-Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+Your game will be played by a human player. 
+You will write a function that takes the user choice and returns it.
 
-Your game will keep track of the players score. You will write variables to keep track of the players score.
+Your game will keep track of the players score. 
+You will write variables to keep track of the players score.
 
-Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+Your game will be played round by round. 
+You will write a function that takes the human and computer player choices as arguments, 
+plays a single round, increments the round winner’s score and logs a winner announcement.
+
+In our UI, the player should be able to play the game by clicking on buttons, 
+rather than typing their answer in a prompt.
 
 */
 
 let humanScore = 0;
 let computerScore = 0;
+let computerPickDisplay = document.querySelector("#computer-pick")
+let scoreDisplay = document.querySelector("#score-display")
+let scoreStore = ""
+let rockBtn = document.querySelector("#btn-rock")
+let paperBtn = document.querySelector("#btn-paper")
+let scissorsBtn = document.querySelector("#btn-scissors")
+
+
 
 // getComputerChoice() returns either Rock, Paper or Scissors at random using the Math.random function.
 function getComputerChoice() {
@@ -20,78 +36,83 @@ function getComputerChoice() {
     return playWords[randomNum];
 }
 
-//Prompts the user for a choice, handles the case sensitive issues.
-function getHumanChoice(){
-    let userChoicePromt = prompt("Input Rock, Paper or Scissors");
-    userChoice = userChoicePromt.toLowerCase();
-
-    if (userChoice == 'rock'){
-        return 'Rock';
-    }
-    else if (userChoice == 'paper'){
-        return 'Paper';
-    }
-    else if (userChoice == 'scissors'){
-        return 'Scissors';
-    }
-    else{
-        return 'Please enter valid input';
-    }
-}
-
-//the playRound(args1, args2) function runs the logic, checking the 7 posiblities with if else condtional statements.
+//the playRound(args1, args2) function runs the logic, 
+// checking the 7 posiblities with if else condtional statements.
 function playRound(humanChoice, computerChoice){
     if (humanChoice == computerChoice){
-        console.log("Thats a tie!");
+        scoreStore = "Thats a tie!"
+
     }
     else if (humanChoice == 'Rock' && computerChoice == 'Paper'){
-        console.log("You lose! Paper Beats Rock");
+        scoreStore = "You lose! Paper Beats Rock"
         computerScore +=1; 
     }
     else if (humanChoice == 'Rock' && computerChoice == 'Scissors'){
-        console.log("You Win! Rock Beats Scissors");
+        scoreStore = "You Win! Rock Beats Scissors"
         humanScore +=1; 
     }
     else if (humanChoice == 'Paper' && computerChoice == 'Rock'){
-        console.log("You Win! Paper Beats Rock");
+        scoreStore = "You Win! Paper Beats Rock"
         humanScore +=1; 
     }
     else if (humanChoice == 'Paper' && computerChoice == 'Scissors'){
-        console.log("You lose! Scissors Beats Paper");
+        scoreStore = "You lose! Scissors Beats Paper"
         computerScore +=1; 
     }
     else if (humanChoice == 'Scissors' && computerChoice == 'Rock'){
-        console.log("You lose! Rock Beats Scissors");
+        scoreStore = "You lose! Rock Beats Scissors"
         computerScore +=1;
     }
     else if (humanChoice == 'Scissors' && computerChoice == 'Paper'){
-        console.log("You Win! Scissors Beats Paper");
+        scoreStore = "You Win! Scissors Beats Paper"
         humanScore +=1;
     }
-    else{
-        console.log("Invalid input!")
-    }
+
+    return scoreStore
+
 }
 
-//playGame() function uses a for loop which then calls getHumanChoice() and getComputerChoice(), taking their respective return values and passing them to the playRound(args1, args2).
-//Once the loop exists, the function compares the scores and gives a final winner or loser announcment.
+//the playGame() function checks if either one of the players have reached a score of 5; if they have then
+// it displays a message and disables the player's buttons.
+
 function playGame(){
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        continue;
-    }
-    if (humanScore > computerScore){
-        console.log("Congratulation you have won against the computer!");
-    }
-    else if (humanScore < computerScore){
-        console.log("Sorry, you have lost against the computer!");      
-    }
-    else if (humanScore == computerScore){
-        console.log("It a draw!");
+    if ((humanScore >= 5) || (computerScore >= 5)){
+        if (humanScore > computerScore){
+            scoreStore = "Congratulation you have won against the computer!"
+        }
+        else if (humanScore < computerScore){
+            scoreStore = "Sorry, you have lost against the computer!"
+        }
+        else if (humanScore == computerScore){
+            scoreStore = "It a draw!"
+        }
+        rockBtn.disabled = true
+        paperBtn.disabled = true
+        scissorsBtn.disabled = true
+
+        return scoreStore
     }
 }
 
-// The play game function is called to run the game.
-playGame();
+// Each button calls on the playRound() function and the playGame() function.
+rockBtn.addEventListener('click', function(){
+    playRound('Rock', getComputerChoice())
+    playGame()
+    scoreDisplay.innerHTML = `${scoreStore} <br> Computer: ${computerScore} <br> Your Score: ${humanScore}`
+})
+
+paperBtn.addEventListener('click', function(){
+    playRound('Paper', getComputerChoice())
+    playGame()
+    scoreDisplay.innerHTML = `${scoreStore} <br> Computer: ${computerScore} <br> Your Score: ${humanScore}`
+})
+scissorsBtn.addEventListener('click', function(){
+    playRound('Scissors', getComputerChoice())
+    playGame()
+    scoreDisplay.innerHTML = `${scoreStore} <br> Computer: ${computerScore} <br> Your Score: ${humanScore}`
+})
+
+
+
+
+
